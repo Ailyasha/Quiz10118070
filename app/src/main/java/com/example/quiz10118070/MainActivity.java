59913cd44918daa634eace9496e3f5c76303ea2f
aufa <aufa.ilyasha@gmail.com>
2021-05-29 11:18:47 +0700

    Initial commit

---
+++ app/src/main/java/com/example/quiz10118070/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.quiz10118070;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+//10118070
+//Aufa Ilyasha
+//IF-2
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
